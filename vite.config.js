fcff957f1920af768410c4f5c5700c14f3f9adb7
DHONDGE YASHVARDHAN CHATURBHUJ <152969155+Yashvardhandhondge@users.noreply.github.com>
2024-09-16 20:13:22 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -5,26 +5,5 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  build: {
-    rollupOptions: {
-      external: [
-        'crypto',
-        'http',
-        'https',
-        'zlib',
-        'url',
-        'stream',
-      ],
-    },
-  },
-  resolve: {
-    alias: {
-      'crypto': 'crypto-browserify',
-      'http': 'stream-http',
-      'https': 'https-browserify',
-      'zlib': 'browserify-zlib',
-      'url': 'url',
-      'stream': 'stream-browserify',
-    },
-  },
+
 });
