a64e11886fdfd8165bfc025483ef4a6798ecb32b
DHONDGE YASHVARDHAN CHATURBHUJ <152969155+Yashvardhandhondge@users.noreply.github.com>
2024-09-16 19:52:55 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [
+        'crypto',
+        'http',
+        'https',
+        'zlib',
+        'url',
+        'stream',
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      'crypto': 'crypto-browserify',
+      'http': 'stream-http',
+      'https': 'https-browserify',
+      'zlib': 'browserify-zlib',
+      'url': 'url',
+      'stream': 'stream-browserify',
+    },
+  },
+});
